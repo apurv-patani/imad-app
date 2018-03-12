a9ad589979447bcd7667d41edcd020e69e8044b7
apurv-patani <30462443+apurv-patani@users.noreply.github.com>
2018-03-12 15:28:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,18 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+app.get('/article_one', function (req, res) {
+  res.sendFile("Article one it is. Believe it.");
+});
+
+app.get('/article_two', function (req, res) {
+  res.sendFile("Article two it is. Believe it.");
+});
+
+app.get('/article_three', function (req, res) {
+  res.sendFile("Article three it is. Believe it.");
+});
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
