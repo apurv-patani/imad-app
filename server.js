018b4c9a0f8b236bf6c8f6ee7cdf28e09e9be16e
apurv-patani <30462443+apurv-patani@users.noreply.github.com>
2018-03-13 20:02:16 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -91,7 +91,7 @@ app.get('/article_three', function (req, res) {
 
 app.get('/:articleName', function (req, res) {
     var articleName = req.params.articleName;
-    res.send(createTemplate(articles[articleName]));
+    res.send(createTemplate(articles.articleName));
 });
 
 
