2a7197e6edd7cbfe0473d76733e88906dae05f55
Apurv Patani <patani.apurv@gmail.com>
2018-03-14 17:02:01 +0530

    Made changes to main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,20 @@
 console.log('Loaded!');
+
+//change text from main-text div
+var element = document.getElementById('main-text');
+element.innerHTML = 'new val';
+
+//change posn of image 
+
+var img=document.getElementById('madi');
+var marginLeft = 0; 
+function moveRight()
+{
+	marginLeft+=5;
+	img.style.marginLeft=marginLeft + 'px';
+}
+img.onclick = function ()
+{
+	var interval = setInterval(moveRight,100);
+	// img.style.marginLeft = '100px';
+};
